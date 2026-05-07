374246b42a4da92986c4b34a7cc7f288acee5349
Raul Somesan <raulfabiansome@gmail.com>
2026-05-07 10:29:20 +0300

    Namespaces

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,19 @@
 <?php
 
-//phpinfo();
+require_once('../PaymentGateway/Paddle/Transaction.php');
+require_once('../PaymentGateway/Paddle/CustomerProfile.php');
+require_once('../PaymentGateway/Paddle/DateTime.php');
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+require_once('../Notification/Email.php');
+
+require_once('../PaymentGateway/Stripe/Transaction.php');
+
+//use PaymentGateway\Paddle\{Transaction, CustomerProfile};
+use PaymentGateway\Paddle AS PA;
+use PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+$paddleTransaction = new PA\Transaction();
+$stripeTransaction = new StripeTransaction();
+$paddleCustomerProfile = new PA\CustomerProfile();
+
+var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
