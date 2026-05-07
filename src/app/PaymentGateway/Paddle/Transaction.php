bff2fb4cd625c566dade674a7e83924c6545a5b4
Raul Somesan <raulfabiansome@gmail.com>
2026-05-07 12:34:24 +0300

    Static Variables & Methods

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -8,13 +8,17 @@ use App\Enums\Stauts;
 
 class Transaction
 {
+    
+    private static int $count = 0;
+    private string $status = 'pending';
 
     public function __construct
     (
-        private string $status = 'pending'
+        public float $amount,
+        public string $description,
     )
     {
-        var_dump(Stauts::PAID);
+        self::$count++;
     }
 
     public function setStatus(string $status)
@@ -25,5 +29,15 @@ class Transaction
         }
         $this->status = $status;
         return $this;
+    }
+
+    public static function getCount()
+    {
+        return self::$count;
+    }
+
+    public function process()
+    {
+        echo 'Processing paddle transaction...';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\DB;
 use App\Enums\Stauts;
 use App\PaymentGateway\Paddle\Transaction;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$transaction = new Transaction();
+$transaction1 = new Transaction(25, 'Transaction 1');
+$transaction1->setStatus(Stauts::PAID);
 
-$transaction->setStatus(Stauts::PAID);
+var_dump(Transaction::getCount());
 
-var_dump($transaction);
+$db = DB::getInstance([]);
+$db = DB::getInstance([]);
+$db = DB::getInstance([]);
+$db = DB::getInstance([]);
