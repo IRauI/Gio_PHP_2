1f4f3b871cc4fd812bdb89762f798828ba0fb629
Raul Somesan <raulfabiansome@gmail.com>
2026-05-18 13:49:02 +0300

    GET & POST

--- src/public/index.php
+++ src/public/index.php
@@ -5,8 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $router = new App\Router();
 
 $router
-    ->register('/',[App\Controller\Home::class, 'index'])
-    ->register('/invoices',[App\Controller\Invoice::class, 'index'])
-    ->register('/invoices/create',[App\Controller\Invoice::class, 'create']);
+    ->get('/',[App\Controller\Home::class, 'index'])
+    ->get('/invoices',[App\Controller\Invoice::class, 'index'])
+    ->get('/invoices/create',[App\Controller\Invoice::class, 'create'])
+    ->post('/invoices/create',[App\Controller\Invoice::class, 'store']);
 
-echo $router->resolve($_SERVER['REQUEST_URI']);
+echo $router->resolve($_SERVER['REQUEST_URI'],strtolower($_SERVER['REQUEST_METHOD']));
