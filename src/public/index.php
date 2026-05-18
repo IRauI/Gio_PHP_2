9114b8a3f749ef6dd7ad3781a50f8023c5144b19
Raul Somesan <raulfabiansome@gmail.com>
2026-05-18 11:58:24 +0300

    PHP OOP

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\DB;
-use App\Enums\Stauts;
-use App\PaymentGateway\Paddle\Transaction;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$transaction1 = new Transaction(25);
-$transaction1->process();
+echo '<pre>';
+var_dump($_SERVER);
+echo '</pre>';
